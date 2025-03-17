9d92d2da5e062d7c208b95c1387a65fda72eae5d
heechy33 <heechany0321@gmail.com>
2025-03-17 02:45:41 +0000

    first commit

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -126,6 +126,7 @@ void print_word_ladder(const vector<string>& ladder) {
         for (size_t i = 0; i < ladder.size(); ++i) {
             cout << ladder[i] << " ";
         }
+        cout << endl;
     } else {
         cout << "No word ladder found.";
     }
